57c4ee31b4fbd91c45e870e0cf95b93b76d44d61
Rodrigo Leiroz <rodrigoleiroz.devops@gmailcom>
2023-09-13 23:41:37 -0300

    Adiciona App.js chama Login

--- trabalho_final/tela_login.js
+++ trabalho_final/tela_login.js
@@ -1,19 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Image, ScrollView, Button, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View, TextInput, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import tela_cadastro from './tela_cadastro';
 
-
-
-export default function App() {
-
+export default function tela_login() {
   const [usuario, setUsuario] = useState('');
   const [senha, setSenha] = useState('');
+  const [erroUsuario, setErroUsuario] = useState(''); // Estado para a mensagem de erro
+  const navigation = useNavigation(); // Obtenha o objeto de navegação
 
-  
-  
+  const navigateToAlterarSenha = () => {
+    // Navegue para a tela 'altera_senha'
+    navigation.navigate('altera_senha');
+  };
+
+  const navigateToTelaCadastro = () => {
+    // Navegue para a tela 'tela_cadastro'
+    navigation.navigate('tela_cadastro');
+  };
+
   const showAlert = () => {
+    // Verifique se o email é válido antes de fazer o login
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(usuario)) {
+      setErroUsuario('Digite um email válido!');
+      setUsuario('');
+      setSenha('');       
+      return; // Saia da função se o email não for válido
+    }
+
     window.alert('Seu login foi realizado com sucesso!');
+    setUsuario('');
+    setSenha(''); 
   };
 
   return (
@@ -27,34 +45,59 @@ export default function App() {
       />
 
       <Text style={styles.display1}></Text>
-      
+
       <Text style={styles.display2}></Text>
       <TextInput
-        placeholder= "Email do usuario"
+        placeholder="Email do usuario"
         style={styles.usuario}
         value={usuario}
         onChangeText={(texto) => setUsuario(texto)}
       />
+      <TextInput
+        onChangeText={(texto) => {
+          setUsuario(texto);
+          const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+          if (!emailRegex.test(texto)) {
+            setErroUsuario('Digite um email válido');
+          } else {
+            setErroUsuario('');
+          }
+        }}
+      />
+      {erroUsuario ? (
+        <Text style={styles.erro}>{erroUsuario}</Text>
+      ) : null}
 
       <Text style={styles.display3}></Text>
       <TextInput
         secureTextEntry={true}
-        placeholder= "Senha"
+        placeholder="Senha"
         style={styles.senha}
         value={senha}
-        onChangeText={(texto) => setSenha(texto)}        
+        onChangeText={(texto) => setSenha(texto)}
       />
-            
-      <TouchableOpacity style={styles.botaoLogin} onPress={showAlert} >
-        <Text style= {{COLOR:'white', textAlign:'center'}}>Login</Text>        
+
+      <View style={styles.linkContainer}>
+        <TouchableOpacity style={[styles.link, { alignSelf: 'flex-start' }]} onPress={navigateToAlterarSenha}>
+          <Text style={{ color: 'blue', textAlign: 'center' }}>Alterar senha</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.link, { alignSelf: 'flex-start' }]} onPress={navigateToTelaCadastro}>
+          <Text style={{ color: 'blue', textAlign: 'center' }}>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.botaoLogin} onPress={showAlert}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>Login</Text>
       </TouchableOpacity>
 
       <ScrollView style={styles.scrollView}>
         <Text style={styles.text}></Text>
       </ScrollView>
 
-      <StatusBar style='auto' />
+      
     </View>
+
   );
 }
 
@@ -85,6 +128,12 @@ const styles = StyleSheet.create({
     marginTop: 10,
     padding: 15,
   },
+
+  erro: {
+    color: 'yellow',
+    marginLeft: 15,
+    },
+  
   senha: {
     backgroundColor: '#FFF',
     borderWidth: 1,
@@ -95,7 +144,12 @@ const styles = StyleSheet.create({
     marginTop: 10,
     padding: 15,
   },
-  
+
+  link: {
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
   botaoLogin: {
     width: 350,
     height: 20,
@@ -105,7 +159,6 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginTop: 10,
     justifyContent: 'center',
-
   },
 
   imagem: {
