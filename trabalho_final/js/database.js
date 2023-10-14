2b9b9ecaa69fba689c79423c7504fecbe51021e1
rodrigo leiroz <rodrigoleiroz.devops@gmail.com>
2023-10-14 11:30:18 -0300

    Change host and table name

--- trabalho_final/js/database.js
+++ trabalho_final/js/database.js
@@ -2,7 +2,7 @@ const pg = require('pg')
 
 const client = new pg.Client({
     user: 'postgres',
-    host: '192.168.0.62',
+    host: 'localhost',
     database: 'cadastro',
     password: 'psicologia',
     port: 5432,
