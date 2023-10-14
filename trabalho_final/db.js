9cba2ed7230e2a1c4c58582f0d9b10206e05ea43
rodrigo leiroz <rodrigoleiroz.devops@gmail.com>
2023-10-14 11:29:02 -0300

    Add function insert

--- trabalho_final/db.js
+++ trabalho_final/db.js
@@ -29,8 +29,15 @@ async function connect() {
         const client = await connect();
         return await client.query('DELETE FROM usuarios WHERE celular = $1', [celular]);
     }
+
+    async function insertCustomer(customer) {
+        const client = await connect();
+        const sql = 'INSERT INTO usuarios(primeironome, sobrenome, email, celular, senha) VALUES ($1,$2,$3,$4,$5);';
+        const values = [customer.primeironome, customer.sobrenome, customer.email, customer.celular, customer.senha];
+        return await client.query(sql, values);
+    }
     
-    module.exports = { selectCustomers, selectCustomer, deleteCustomer }
+    module.exports = { selectCustomers, selectCustomer, deleteCustomer, insertCustomer }
     
     //apenas testando a conexão
     const client = await pool.connect();
